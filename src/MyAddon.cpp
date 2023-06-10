aabf20aad432df0c173a44da2f2f13486bf3d580
alex-gudkov <gudkov.bsuir@gmail.com>
2023-06-10 22:29:45 +0300

    add sum function

--- src/MyAddon.cpp
+++ src/MyAddon.cpp
@@ -7,9 +7,33 @@ void GetHello(const v8::FunctionCallbackInfo<v8::Value> &args)
     args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "Hello").ToLocalChecked());
 }
 
+void Sum(const v8::FunctionCallbackInfo<v8::Value> &args)
+{
+    v8::Isolate *isolate = args.GetIsolate();
+
+    if (args.Length() != 2)
+    {
+        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
+
+        return;
+    }
+
+    if (!args[0]->IsNumber() || !args[1]->IsNumber())
+    {
+        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong arguments type").ToLocalChecked()));
+
+        return;
+    }
+
+    double value = args[0].As<v8::Number>()->Value() + args[1].As<v8::Number>()->Value();
+
+    args.GetReturnValue().Set(value);
+}
+
 void Initialize(v8::Local<v8::Object> exports)
 {
     NODE_SET_METHOD(exports, "getHello", GetHello);
+    NODE_SET_METHOD(exports, "sum", Sum);
 }
 
 NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 const myAddon = require('../build/Release/my_addon.node');
 
 console.log(myAddon.getHello());
+
+console.log(myAddon.sum(1, 2));
